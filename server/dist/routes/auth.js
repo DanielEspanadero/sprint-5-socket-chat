72a702a7a466fcd26b548f923ead9aa1b1928935
Daniel Españadero <developer@daniel-espanadero.com>
2022-04-15 23:24:46 +0200

    Google route added

--- server/dist/routes/auth.js
+++ server/dist/routes/auth.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const auth_1 = require("../controllers/auth");
 const router = (0, express_1.Router)();
+router.post('/google', auth_1.googleLogin);
+// Create new users
 //# sourceMappingURL=auth.js.map
--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import { check } from "express-validator";
 
-import { createUser, login, renewToken } from "../controllers/auth";
+import { createUser, login, renewToken, googleLogin } from "../controllers/auth";
+import { checkFields } from "../middlewares/validate-fields";
+import { validateToken } from "../middlewares/validate-jwt";
 
 const router = Router();
+
+router.post('/google', googleLogin);
+
+// Create new users
 
