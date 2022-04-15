412e7ec05a10f4907a892b8b56651359691bb661
Daniel Españadero <developer@daniel-espanadero.com>
2022-04-15 19:49:03 +0200

    User schema created

---
+++ server/models/user.ts
@@ -0,0 +1,48 @@
+import {Schema, model} from 'mongoose';
+
+export interface IUser extends Document {
+    email: string;
+    password: string;
+    online: boolean;
+    comparePassword: (password: string) => Promise<Boolean>
+};
+
+const UserSchema = new Schema ({
+    firstName: {
+        type: String,
+        required: [true, 'First name is required.']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name is required.']
+    },
+    email: {
+        type: String,
+        required:[true, 'Email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required.']
+    },
+    online: {
+        type: Boolean,
+        default: false
+    },
+    avatar: {
+        type: String,
+        required: [true, 'Avatar is required.']
+    },
+    auth: {
+        type: Object,
+        required: false
+    }
+});
+
+UserSchema.method('toJSON', function () {
+    const { __v, _id, password, ...object } = this.toObject();
+    object.uid = _id;
+    return object;
+});
+
+export const User = model<IUser>('User', UserSchema);
