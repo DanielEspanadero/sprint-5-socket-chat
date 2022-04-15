a7b5379b0eec66d5d0ba7892c58e0587dd1d7fd6
Daniel Españadero <developer@daniel-espanadero.com>
2022-04-15 22:23:00 +0200

    JWT generated

--- server/dist/helpers/generate-jwt.js
+++ server/dist/helpers/generate-jwt.js
@@ -1,4 +1,51 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.checkJWT = exports.generateAccessToken = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 require("dotenv/config");
+const generateAccessToken = (uid = '') => {
+    return new Promise((resolve, reject) => {
+        const user = uid;
+        jsonwebtoken_1.default.sign(user, process.env.SECRETORPRIVATEKEY, {
+            expiresIn: '4h'
+        }, (err, token) => {
+            if (err) {
+                console.log(err);
+                reject('Failed to generate JWT');
+            }
+            else {
+                resolve(token);
+            }
+            ;
+        });
+    });
+};
+exports.generateAccessToken = generateAccessToken;
+const checkJWT = (token = '') => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        if (token.length < 10) {
+            return null;
+        }
+        ;
+        const { uid } = jsonwebtoken_1.default.verify(token, process.env.SECRETORPRIVATEKEY);
+        return [true, uid];
+    }
+    catch (error) {
+        console.log(error);
+        return [false, null];
+    }
+});
+exports.checkJWT = checkJWT;
 //# sourceMappingURL=generate-jwt.js.map
--- server/helpers/generate-jwt.ts
+++ server/helpers/generate-jwt.ts
@@ -1,4 +1,36 @@
-import jwt, {JwtPayload} from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import 'dotenv/config';
 import { User } from '../models/user';
 
+export const generateAccessToken = (uid = '') => {
+    return new Promise((resolve: any, reject: any) => {
+        const user = uid;
+
+        jwt.sign(user, process.env.SECRETORPRIVATEKEY as string, {
+            expiresIn: '4h'
+        }, (err, token) => {
+            if (err) {
+                console.log(err);
+                reject('Failed to generate JWT')
+            } else {
+                resolve(token);
+            };
+        }
+        );
+    });
+};
+
+export const checkJWT = async (token: string = '') => {
+    try {
+
+        if (token.length < 10) {
+            return null;
+        };
+        const { uid }: any = jwt.verify(token, process.env.SECRETORPRIVATEKEY as string);
+
+        return [true, uid]
+    } catch (error) {
+        console.log(error);
+        return [false, null];
+    }
+}
