02fd054480d6032b466cd2d7f2b273ade884f278
Daniel Españadero <developer@daniel-espanadero.com>
2022-04-18 12:40:31 +0200

    ChatSelect component created

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -1,1 +1,2 @@
 import React, { useContext } from 'react';
+import { ChatSelect } from '../components/ChatSelect';
---
+++ client/src/router/App.tsx
@@ -0,0 +1,3 @@
+import React, { useContext, useEffect } from "react";
+// import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+
