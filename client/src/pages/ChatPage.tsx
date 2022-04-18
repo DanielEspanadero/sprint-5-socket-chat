c07924d937ea701e598118197c7fc8c19a03f604
Daniel Españadero <developer@daniel-espanadero.com>
2022-04-18 13:17:52 +0200

    update

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -1,3 +1,4 @@
 import React, { useContext } from 'react';
 import { ChatSelect } from '../components/ChatSelect';
 import { Sidebar } from '../components/sidebar/Sidebar';
+import { MessagesList } from '../components/Body/Chat';
